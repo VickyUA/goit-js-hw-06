2c41ff5fdeaad8b13b46306059d0ff548e8ab3f8
VickyUA <vickyy379@gmail.com>
2023-09-29 19:18:26 +0200

    9 tasks done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
+const formLogin = document.querySelector('.login-form');
 
+formLogin.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const userEmail = form.elements.email.value;
+    const userPassword = form.elements.password.value;
+
+    if (userEmail === "" || userPassword === "") {
+        return alert("Усі поля повинні бути заповнені!");
+    }
+
+    const { email, password } = form.elements;
+
+    const data = {
+        email: email.value,
+        password: password.value
+    }
+
+    console.log(data);
+
+    form.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,19 @@
+const currentColor = document.querySelector('.color');
+
+const colorBtn = document.querySelector('.change-color');
+
+const body = document.body;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+
+colorBtn.addEventListener('click', handleClick);
+
+function handleClick(event) {
+  body.style.backgroundColor = getRandomHexColor(); 
+  currentColor.textContent = getRandomHexColor(); 
 }
